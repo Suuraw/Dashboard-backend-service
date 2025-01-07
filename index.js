ec7e6c3fa6663ddde82fcb3ca52d9e9061b3b808
Sujay kumar <sujayraw13@gmail.com>
2025-01-07 17:39:35 +0530

    write user login logic

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT||3000;
 app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
-app.use("/admin",authRoutes);
+app.use("/auth",authRoutes);
 app.use("/api",crudRoute);
 
 app.listen(port, () => {
--- kaduRoute/authRoutes.js
+++ kaduRoute/authRoutes.js
@@ -1,5 +1,7 @@
 import { loginAdmin } from "../controllerLogic/loginAdmin.js";
+import { loginUser } from "../controllerLogic/loginUser.js";
 import express from "express";
 const router = express.Router();
 router.post("/login", loginAdmin);
+router.post("/userLogin", loginUser);
 export default router;
