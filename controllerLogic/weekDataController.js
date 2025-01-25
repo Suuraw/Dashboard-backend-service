7f5699db7a7f006928e51c9ccdc9521128a21670
Sujay kumar <sujayraw13@gmail.com>
2025-01-25 13:40:57 +0530

    add week delete route and new week get and post route logic

--- controllerLogic/weekDataController.js
+++ controllerLogic/weekDataController.js
@@ -204,7 +204,246 @@ export const addWeek5Data = async (req, res) => {
     return res.status(500).json({ message: "Server not found" });
   }
 };
+// week 6
+export const getWeek6Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 6");
+    const course = await collection.findOne({});
 
+    if (course) {
+      return res.status(200).json(course);
+    } else {
+      return res.status(200).json({ message: "No course document found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const addWeek6Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 6");
+
+    const weekData = req.body;
+
+    await collection.deleteMany({});
+    const result = await collection.insertOne({ weekData });
+    if (result.acknowledged) {
+      return res.status(201).json({
+        success: true,
+        message: "Course data added successfully",
+        data: weekData,
+      });
+    } else {
+      return res.status(500).json({ message: "insertion failed" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Server not found" });
+  }
+};
+//week 7
+export const getWeek7Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 7");
+    const course = await collection.findOne({});
+
+    if (course) {
+      return res.status(200).json(course);
+    } else {
+      return res.status(200).json({ message: "No course document found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const addWeek7Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 7");
+
+    const weekData = req.body;
+
+    await collection.deleteMany({});
+    const result = await collection.insertOne({ weekData });
+    if (result.acknowledged) {
+      return res.status(201).json({
+        success: true,
+        message: "Course data added successfully",
+        data: weekData,
+      });
+    } else {
+      return res.status(500).json({ message: "insertion failed" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Server not found" });
+  }
+};
+//week 8
+export const getWeek8Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 8");
+    const course = await collection.findOne({});
+
+    if (course) {
+      return res.status(200).json(course);
+    } else {
+      return res.status(200).json({ message: "No course document found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const addWeek8Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 8");
+
+    const weekData = req.body;
+
+    await collection.deleteMany({});
+    const result = await collection.insertOne({ weekData });
+    if (result.acknowledged) {
+      return res.status(201).json({
+        success: true,
+        message: "Course data added successfully",
+        data: weekData,
+      });
+    } else {
+      return res.status(500).json({ message: "insertion failed" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Server not found" });
+  }
+};
+//week 9
+export const getWeek9Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 9");
+    const course = await collection.findOne({});
+
+    if (course) {
+      return res.status(200).json(course);
+    } else {
+      return res.status(200).json({ message: "No course document found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const addWeek9Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 9");
+
+    const weekData = req.body;
+
+    await collection.deleteMany({});
+    const result = await collection.insertOne({ weekData });
+    if (result.acknowledged) {
+      return res.status(201).json({
+        success: true,
+        message: "Course data added successfully",
+        data: weekData,
+      });
+    } else {
+      return res.status(500).json({ message: "insertion failed" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Server not found" });
+  }
+};
+//week 10
+export const getWeek10Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 10");
+    const course = await collection.findOne({});
+
+    if (course) {
+      return res.status(200).json(course);
+    } else {
+      return res.status(200).json({ message: "No course document found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const addWeek10Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 10");
+
+    const weekData = req.body;
+
+    await collection.deleteMany({});
+    const result = await collection.insertOne({ weekData });
+    if (result.acknowledged) {
+      return res.status(201).json({
+        success: true,
+        message: "Course data added successfully",
+        data: weekData,
+      });
+    } else {
+      return res.status(500).json({ message: "insertion failed" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Server not found" });
+  }
+};
+//week 11
+export const getWeek11Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 11");
+    const course = await collection.findOne({});
+
+    if (course) {
+      return res.status(200).json(course);
+    } else {
+      return res.status(200).json({ message: "No course document found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const addWeek11Data = async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db("Courses");
+    const collection = db.collection("Week 11");
+
+    const weekData = req.body;
+
+    await collection.deleteMany({});
+    const result = await collection.insertOne({ weekData });
+    if (result.acknowledged) {
+      return res.status(201).json({
+        success: true,
+        message: "Course data added successfully",
+        data: weekData,
+      });
+    } else {
+      return res.status(500).json({ message: "insertion failed" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Server not found" });
+  }
+};
 //capstone project handler
 export const getCapstoneData = async (req, res) => {
   try {
@@ -317,4 +556,21 @@ export const deleteAssessmentData = async (req, res) => {
       return res.status(404).json({ message: "Document not found" });
     }
   } catch (error) {}
+};
+
+// logic for deleting a specific week data
+export const deleteWeekData = async (req, res) => {
+  try {
+
+    const { week } = req.body;
+    await client.connect();
+    const db = client.db("Courses");
+    const collection=db.collection(week);
+    await collection.deleteMany({});
+    return res
+      .status(200)
+      .json({ message: `Successfully deleted ${week} data` });
+  } catch (error) {
+    return res.status(404).json({ message: `Try Block failed ` });
+  }
 };
--- kaduRoute/crudRoute.js
+++ kaduRoute/crudRoute.js
@@ -1,30 +1,81 @@
-import {getWeek1Data,addWeek1Data,getWeek2Data,addWeek2Data,getWeek3Data,addWeek3Data,getWeek4Data,addWeek4Data,getWeek5Data,addWeek5Data,getCapstoneData,addCapstoneData,getAssessmentData,addAssessmentData,deleteCapstoneData,deleteAssessmentData} from "../controllerLogic/weekDataController.js";
+import {
+  getWeek1Data,
+  addWeek1Data,
+  getWeek2Data,
+  addWeek2Data,
+  getWeek3Data,
+  addWeek3Data,
+  getWeek4Data,
+  addWeek4Data,
+  getWeek5Data,
+  addWeek5Data,
+  getWeek6Data,
+  addWeek6Data,
+  getWeek7Data,
+  addWeek7Data,
+  getWeek8Data,
+  addWeek8Data,
+  getWeek9Data,
+  addWeek9Data,
+  getWeek10Data,
+  addWeek10Data,
+  getWeek11Data,
+  addWeek11Data,
+  getCapstoneData,
+  addCapstoneData,
+  getAssessmentData,
+  addAssessmentData,
+  deleteCapstoneData,
+  deleteAssessmentData,
+  deleteWeekData
+} from "../controllerLogic/weekDataController.js";
 import express from "express";
 import { fetchLeadboard } from "../controllerLogic/leadboardData.js";
-const router=express.Router();
+const router = express.Router();
 // week 1 routes
-router.get("/week1Data",getWeek1Data);
-router.post("/week1",addWeek1Data);
+router.get("/week1Data", getWeek1Data);
+router.post("/week1", addWeek1Data);
 // week 2 routes
-router.get("/week2Data",getWeek2Data);
-router.post("/week2",addWeek2Data);
+router.get("/week2Data", getWeek2Data);
+router.post("/week2", addWeek2Data);
 // week 3 routes
-router.get("/week3Data",getWeek3Data);
-router.post("/week3",addWeek3Data);
+router.get("/week3Data", getWeek3Data);
+router.post("/week3", addWeek3Data);
 // week 4 routes
-router.get("/week4Data",getWeek4Data);
-router.post("/week4",addWeek4Data);
+router.get("/week4Data", getWeek4Data);
+router.post("/week4", addWeek4Data);
 // week 5 routes
-router.get("/week5Data",getWeek5Data);
-router.post("/week5",addWeek5Data);
+router.get("/week5Data", getWeek5Data);
+router.post("/week5", addWeek5Data);
+//week 6 routes
+router.get("/week6Data", getWeek6Data);
+router.post("/week6", addWeek6Data);
+// week 7 routes
+router.get("/week7Data", getWeek7Data);
+router.post("/week7", addWeek7Data);
+// week 8 routes
+router.get("/week8Data", getWeek8Data);
+router.post("/week8", addWeek8Data);
+// week 9 routes
+router.get("/week9Data", getWeek9Data);
+router.post("/week9", addWeek9Data);
+// week 10 routes
+router.get("/week10Data", getWeek10Data);
+router.post("/week10", addWeek10Data);
+// week 11 routes
+router.get("/week11Data", getWeek11Data);
+router.post("/week11", addWeek11Data);
 // capstone route
-router.get("/capstoneData",getCapstoneData);
-router.post("/capstone",addCapstoneData);
-router.delete("/deleteCapstoneData/:id",deleteCapstoneData);
+router.get("/capstoneData", getCapstoneData);
+router.post("/capstone", addCapstoneData);
+router.delete("/deleteCapstoneData/:id", deleteCapstoneData);
 // assessment routes
-router.get("/assessmentData",getAssessmentData);
-router.post("/assessment",addAssessmentData);
-router.delete("/deleteAssessmentData/:id",deleteAssessmentData);
+router.get("/assessmentData", getAssessmentData);
+router.post("/assessment", addAssessmentData);
+router.delete("/deleteAssessmentData/:id", deleteAssessmentData);
 
-router.get("/leadboardData",fetchLeadboard);
+//route to Delete a specific week data 
+router.delete("/deleteWeek",deleteWeekData);
+
+router.get("/leadboardData", fetchLeadboard);
 export default router;
